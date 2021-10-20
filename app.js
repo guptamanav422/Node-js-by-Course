24e745369ac61f189fd8235dd0584dfb92385de4
Manav <guptamanav422@gmail.com>
2021-10-20 17:29:58 +0530

    Express--> adding middleware

--- app.js
+++ app.js
@@ -1,50 +1,22 @@
 const http= require("http");
 const fs=require("fs");
+const express=require('express');
 
-// if request comes then execute this call back function 
-function rqListener(req,res){
-    // console.log(req.url, req.method, req.headers);
-    // process.exit();
+// function 
+const app=express();
 
-    const url=req.url;
-    const method=req.method;
+// executes for every incoming requests 
+app.use((req,res,next)=>{
+    console.log("in the middleware");
+    next(); // allows the request to continue to the next middleware
+});
+app.use((req,res,next)=>{
+    console.log("in the another middleware");
+    //  sending response
+    res.send(`<h1> Hello from Express!`)
+});
 
-    if(url==='/'){
-        res.write('<html>')
-        res.write('<head> <title> Enter Message </title> </head>')
-        res.write('<body> <form action="/message" method="POST"> <input type="text" name="message"/> <button type="submit"> Submit </button> </form> </body>')
-        res.write('</html>')
-        return res.end();
-    }
-    if(url==="/message" && method==='POST'){
+// const server=http.createServer(app);
 
-        const body=[];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk)
-        });
-
-        return req.on('end',()=>{
-            const parsedBody=Buffer.concat(body).toString();
-            const message=parsedBody.split("=")[1];
-            // console.log(parsedBody);
-            // fs.writeFileSync("message.txt",message);
-            fs.writeFile("message.txt",message,err=>{
-                res.statusCode=302;
-                res.setHeader('Location','/');
-                return res.end();
-            })
-        })
-    }
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<head> <title> My First Page </title> </head>')
-    res.write('<body> <h1> Hello from Manav </body>')
-    res.write('</html>')
-    return res.end();
-}
-
-// rqListener always runs after every incoming request 
-const server=http.createServer(rqListener);
-
-server.listen(3000);
+// server.listen(3000);
+app.listen(3000);
